9398dbcbccc2fa2e438acb64fdfe949a17392ac1
ViniciusDiasAmorim <100163241+ViniciusDiasAmorim@users.noreply.github.com>
2022-04-22 10:45:13 -0300

    alterando css e refatorando codigo js

--- main.js
+++ main.js
@@ -22,6 +22,8 @@ function Criptografar(){
         }
     }
     texto = texto.join('');
+  
+
 
     resultado.innerHTML = ` <textarea cols="26" rows="13" class="resultado-cripto">${texto}</textarea> 
     <button id="botao-copiar" onclick="Copiar()">Copiar</button>`;
@@ -29,53 +31,13 @@ function Criptografar(){
 }
 
 function Descriptografar() {
-    console.log("cliquei")
 
-
-    var texto = cripto.value;
-    texto = texto.toLowerCase();
-    texto = texto.split('');
-
-    for(var i = 0; i < texto.length; i++)
-    {
-        if(texto[i] == 'a' && texto[i+1] == 'i')
-        {
-            texto[i] = 'a';
-            texto[i+1] = '';
-        }
-        if(texto[i] == 'e' && texto[i+1] == 'n' && texto[i+2] == 't' && texto[i+3] == 'e' && texto[i+4] == 'r')
-        {
-            texto[i] = 'e';
-            texto[i+1] = '';
-            texto[i+2] = '';
-            texto[i+3] = '';
-            texto[i+4] = '';
-
-        }
-        if(texto[i] == 'i' && texto[i+1] == 'm' && texto[i+2] == 'e' && texto[i+3] == 's')
-        {
-            texto[i] = 'i';
-            texto[i+1] = '';
-            texto[i+2] = '';
-            texto[i+3] = '';
-        }
-        if(texto[i] == 'o' && texto[i+1] == 'b' && texto[i+2] == 'e' && texto[i+3] == 'r')
-        {
-            texto[i] = 'o';
-            texto[i+1] = '';
-            texto[i+2] = '';
-            texto[i+3] = '';
-        }
-        if(texto[i] == 'u' && texto[i+1] == 'f' && texto[i+2] == 'a' && texto[i+3] == 't')
-        {
-            texto[i] = 'u';
-            texto[i+1] = '';
-            texto[i+2] = '';
-            texto[i+3] = '';
-        }
-    }
-
-    texto = texto.join('');
+    texto = cripto.value;
+    texto = texto.replace(/ai/g, "a")
+    texto = texto.replace(/enter/g, "e")
+    texto = texto.replace(/imes/g, "i")
+    texto = texto.replace(/ober/g, "o")
+    texto = texto.replace(/ufat/g, "u")
 
     resultado.innerHTML = ` <textarea cols="26" rows="13" class="resultado-cripto">${texto}</textarea> 
     <button id="botao-copiar" onclick="Copiar()">Copiar</button>`;    
